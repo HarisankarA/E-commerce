24eb0898df0b2c4131403fd48fa4278867cdc24d
harisankar2260@gmail.com <harisankar2260@gmail.com>
2024-07-20 22:42:16 +0530

    Update

---
+++ backend/src/server.js
@@ -0,0 +1,31 @@
+import { config } from 'dotenv'
+import express, { json, urlencoded } from 'express'
+import {set, connect } from 'mongoose'
+
+
+const server = express()
+server.use(json())
+server.use(urlencoded({extended: true}))
+config()
+set('strictQuery', false)
+
+
+
+
+const port = process.env.PORT || 3000
+const mongoose_connection = process.env.MONGO_DB
+
+const start = async () => {
+
+    try {
+        await connect(mongoose_connection,console.log('Connected to MongoDB'))
+        
+        server.listen(port, () => {
+            console.log(`Listening on port ${port}`)
+        })
+    } catch (error) {
+        console.error('Error starting the server:', error)
+        process.exit(1)  // Exit the process with an error code
+    }
+}
+start()
